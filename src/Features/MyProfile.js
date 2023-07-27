842112ac9ef6db0920362d1310fbd166189653a4
lranatunga <rmlik.ranatunga@gmail.com>
2023-07-27 12:14:14 +0100

    check fetching cookies

--- src/Features/MyProfile.js
+++ src/Features/MyProfile.js
@@ -4,6 +4,7 @@ import profile from "../Assets/profile.png"
 import Spinner from "./Spinner"
 import { useEffect, useState } from "react"
 import EditProfile from "../Components/EditProfile"
+import axios from "axios";
 
 export default function MyProfile () {
     const userID = useGetUserID ()
@@ -13,6 +14,15 @@ export default function MyProfile () {
     const [profileUpdate, setProfileUpdate] = useState(false)
     const [saveClick, setSaveClick] = useState(false)
 
+
+    const fetchData = axios.get('https://settlers-hub-server.vercel.app/test-cookie', { withCredentials: true })
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+ 
 
 
     useEffect( () => {
